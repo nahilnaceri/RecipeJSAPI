939d955ac65d3c0329df4c8cd22a91297f7d9a51
nahilnaceri <chafikmihoubnaceri@gmail.com>
2021-01-04 18:54:30 +0100

    load recipe from an apI

--- starter/src/js/controller.js
+++ starter/src/js/controller.js
@@ -1,13 +1,49 @@
-const recipeContainer = document.querySelector('.recipe');
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import * as model from './model.js';
+import RecipeView from './views/recipeView.js';
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
+import icons from 'url:../img/icons.svg';
+
+const recipeContainer = document.querySelector('.recipe');
 
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const renderSpinner = function(parentEl) {
+  const markUp = `<div class="spinner">
+  <svg>
+    <use href="${icons}#icon-loader"></use>
+  </svg>
+</div>`;
+  parentEl.innerHTML = '';
+  parentEl.insertAdjacentHTML('afterbegin', markUp);
+};
+
+const showRecep = async function() {
+  try {
+    const id = window.location.hash.slice(1);
+    if (!id) return;
+    renderSpinner(recipeContainer);
+
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
+    console.log(recipe);
+    RecipeView.render(model.state.recipe);
+    console.log('heeey');
+  } catch (err) {
+    const html = `
+    <div class="error">
+    <div>
+      <svg>
+        <use href="${icons}#icon-alert-triangle"></use>
+      </svg>
+    </div>
+    <p>No recipes found for your query. Please try again!</p>
+  </div> `;
+    recipeContainer.innerHTML = '';
+    recipeContainer.insertAdjacentHTML('beforeend', html);
+  }
+};
+['hashchange', 'load'].forEach(el => window.addEventListener(el, showRecep));
